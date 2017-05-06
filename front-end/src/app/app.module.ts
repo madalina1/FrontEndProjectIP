39aebb4ba829f5766a445ebc15c64a5113ac2c86
Gabriel Rusu <Gabriel Rusu>
2017-05-06 14:06:20 +0300

    adaugat popup cu si fara textfield

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
-import {MaterialModule} from "@angular/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NavbarComponent} from "./components/navbar/navbar.component";
-import {HistoryComponent} from "./components/history/history.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {TransportDocComponent} from "./components/transportDoc/transportDoc.component";
-import {HomeComponent} from "./components/home/home.component";
-import {routing} from "./app.routing";
-import {NgxDatatableModule} from "@swimlane/ngx-datatable";
-import {PopupModule} from 'ng2-opd-popup';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HistoryComponent } from "./components/history/history.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { TransportDocComponent } from "./components/transportDoc/transportDoc.component";
+import { HomeComponent } from "./components/home/home.component";
+import { routing } from "./app.routing";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { AccordionModule } from 'primeng/primeng';     //accordion and accordion tab
+import { MenuItem } from 'primeng/primeng';            //api
+import { DialogModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
@@ -32,9 +34,12 @@ import {PopupModule} from 'ng2-opd-popup';
     BrowserAnimationsModule,
     MaterialModule.forRoot(),
     NgxDatatableModule,
-    PopupModule.forRoot(),
+    AccordionModule,
+    DialogModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
