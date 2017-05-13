328a2dc9ee88e38ec5b2dc4a84d209a4a50c99f7
madalina1 <ioanainsuratelu@yahoo.com>
2017-05-13 11:46:01 +0300

    Am pus o animatie la Loading si am incercat sa il fac responsive, inca este in lucru, pentru a fi responsive.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
-import { AppComponent } from "./app.component";
-import { MaterialModule } from "@angular/material";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { HistoryComponent } from "./components/history/history.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { TransportDocComponent } from "./components/transportDoc/transportDoc.component";
-import { HomeComponent } from "./components/home/home.component";
-import { routing } from "./app.routing";
-import { NgxDatatableModule } from "@swimlane/ngx-datatable";
-import { AccordionModule } from 'primeng/primeng';     //accordion and accordion tab
-import { DialogModule } from 'primeng/primeng';
-import { InputTextModule } from 'primeng/primeng';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HistoryComponent } from './components/history/history.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TransportDocComponent } from './components/transportDoc/transportDoc.component';
+import { HomeComponent } from './components/home/home.component';
+import { routing } from './app.routing';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { AccordionModule, DialogModule, InputTextModule } from 'primeng/primeng'; //accordion and accordion tab
 import { LoginService } from './services/login.service';
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
