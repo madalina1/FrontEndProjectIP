303d19c2382eea63a25475236dff9d53186775d8
mihaiAdascalitei <adascalitei.mihai96@gmail.com>
2017-05-26 18:40:53 +0300

    Login unit test completed model

--- front-end/test/unit/login.spec.js
+++ front-end/test/unit/login.spec.js
@@ -4,7 +4,7 @@ describe('login page testing', function() {
 
     it('invalid e-mail adresses', function() {});
     it('check valid e-mail adresses', function() {});
-    it ('check invalid password ,function()'{});
+    it ('check invalid password ',function(){});
     it ('check valid password',function() {});
     it('corectly submitting', function() { });
 
@@ -22,20 +22,23 @@ describe("Unit Tests", function() {
     function(Services) {
       expect(Services.isValidEmail).not.to.equal(null);
 
-      var EmailIsOk = [
+     /* var EmailIsOk = [
         'adascalitei.mihai96@gmail.com',
         'punk.ripe@yahoo.com',
         'mihai.adascalitei@info.uaic.ro'
-      ];
+      ];*/
 
-      var EmailIsNotOk = [
+      var EmailIsOk[0] = 'adascalitei.mihai96@gmail.com'; /* this is how it should look like */
+
+     /*  var EmailIsNotOk = [
        'adascalitei@.mihai96gmailcom',
                '@punkripe@yahoo.com',
                'mihaia@dascalitei@info.uaicro'
         ''
-      ];
+      ];*/
+*/ no need to chec is not ok emails because we do have just OkEmails */
 
-      for (var i in EmailIsOk) {
+      /*for (var i in EmailIsOk) {
         var ok = Services.checkEmail(EmailIsOk[i]);
         expect(ok).toBeTruthy();
       }
@@ -43,7 +46,10 @@ describe("Unit Tests", function() {
         var ok = Services.checkEmail(EmailIsNotOk[i]);
         expect(ok).toBeFalsy();
       }
+           
+  No loops needed for testing*/
 
+/* no if conditions , no loops , just variables */ 
     }])
   );
 });
